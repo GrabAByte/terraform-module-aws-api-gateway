31046090b73a2b8fd4cb8da45e312774077fb03a
Tom Richards <twr.devops@gmail.com>
2025-05-30 10:22:09 +0100

    feat: initial configuration

--- version.tf
+++ version.tf
@@ -10,13 +10,6 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket  = "grababyte-tfstate"
-    key     = "convertr/convertr.tfstate"
-    region  = "eu-west-2"
-    encrypt = true
-  }
-
   # ubuntu 22.04 used as github runner as it has terraform baked
   # 1.11 latest version available on this build
   required_version = ">= 1.11"
