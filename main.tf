b1ae842790c3e447f0609cc0772d54da27f15765
Tom Richards <twr.devops@gmail.com>
2025-08-07 22:15:03 +0100

    updates

--- main.tf
+++ main.tf
@@ -14,11 +14,12 @@ resource "aws_api_gateway_authorizer" "lambda_auth" {
   identity_source                  = var.api_authorization_identity_source
 }
 
-#resource "aws_api_gateway_resource" "resource" {
-#  rest_api_id = aws_api_gateway_rest_api.api.id
-#  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-#  path_part   = var.api_path_part
-#}
+resource "aws_api_gateway_resource" "resource" {
+  for_each    = var.api_routes
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = each.key
+}
 
 #resource "aws_api_gateway_method" "method" {
 #  rest_api_id   = aws_api_gateway_rest_api.api.id
