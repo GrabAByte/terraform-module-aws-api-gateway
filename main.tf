8bea397d06cc999e5c5793d35899310811a43d3d
Tom Richards <twr.devops@gmail.com>
2025-08-07 20:18:45 +0100

    updates

--- main.tf
+++ main.tf
@@ -4,12 +4,6 @@ resource "aws_api_gateway_rest_api" "api" {
   tags               = var.tags
 }
 
-resource "aws_api_gateway_resource" "resource" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = var.api_path_part
-}
-
 # authentication (time-bound implementation)
 resource "aws_api_gateway_authorizer" "lambda_auth" {
   name                             = "LambdaTokenAuthorizer"
@@ -20,58 +14,66 @@ resource "aws_api_gateway_authorizer" "lambda_auth" {
   identity_source                  = var.api_authorization_identity_source
 }
 
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = var.api_http_method
-  authorization = var.api_authorization_method
-  authorizer_id = aws_api_gateway_authorizer.lambda_auth.id
-}
+#resource "aws_api_gateway_resource" "resource" {
+#  rest_api_id = aws_api_gateway_rest_api.api.id
+#  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+#  path_part   = var.api_path_part
+#}
 
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = var.integration_http_method
-  type                    = var.integration_type
-  uri                     = var.lambda_invoke_arn
-}
+#resource "aws_api_gateway_method" "method" {
+#  rest_api_id   = aws_api_gateway_rest_api.api.id
+#  resource_id   = aws_api_gateway_resource.resource.id
+#  http_method   = var.api_http_method
+#  authorization = var.api_authorization_method
+#  authorizer_id = aws_api_gateway_authorizer.lambda_auth.id
+#}
 
-resource "aws_lambda_permission" "api_gateway" {
-  statement_id  = "AllowExecutionFromAPIGatewayUpload"
-  action        = "lambda:InvokeFunction"
-  function_name = var.lambda_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
-}
+#resource "aws_api_gateway_integration" "lambda" {
+#  rest_api_id             = aws_api_gateway_rest_api.api.id
+#  resource_id             = aws_api_gateway_resource.resource.id
+#  http_method             = aws_api_gateway_method.method.http_method
+#  integration_http_method = var.integration_http_method
+#  type                    = var.integration_type
+#  uri                     = var.lambda_invoke_arn
+#}
 
-resource "aws_api_gateway_deployment" "deployment" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
+## if $lambda_name then
+#resource "aws_lambda_permission" "api_gateway" {
+#  statement_id  = "AllowExecutionFromAPIGatewayUpload"
+#  action        = "lambda:InvokeFunction"
+#  function_name = var.lambda_name
+#  principal     = "apigateway.amazonaws.com"
+#  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+#}
 
-  depends_on = [aws_api_gateway_integration.lambda]
-}
+#resource "aws_api_gateway_deployment" "deployment" {
+#  rest_api_id = aws_api_gateway_rest_api.api.id
+#
+#  depends_on = [aws_api_gateway_integration.lambda]
+#}
 
-resource "aws_api_gateway_stage" "stage" {
-  deployment_id = aws_api_gateway_deployment.deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  stage_name    = var.stage_name
-}
+#resource "aws_api_gateway_stage" "stage" {
+#  deployment_id = aws_api_gateway_deployment.deployment.id
+#  rest_api_id   = aws_api_gateway_rest_api.api.id
+#  stage_name    = var.stage_name
+#}
 
-resource "aws_api_gateway_method_settings" "all" {
-  for_each    = var.api_routes
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = var.stage_name
-  method_path = "${each.value.api_path_part}/${each.value.http_method}"
+#resource "aws_api_gateway_method_settings" "all" {
+#  for_each    = var.api_routes
+#  rest_api_id = aws_api_gateway_rest_api.api.id
+#  stage_name  = var.stage_name
+#  method_path = "${each.value.api_path_part}/${each.value.http_method}"
 
-  # observability
-  # hardening: basic example of controlling number of requests
-  settings {
-    data_trace_enabled = true
-    metrics_enabled    = true
-    # logging_level        = "ERROR" # requires cloud watch setup
-    throttling_burst_limit = 10
-    throttling_rate_limit  = 1
-  }
+# observability
+# hardening: basic example of controlling number of requests
+# variablize
+#  settings {
+#    data_trace_enabled = true
+#    metrics_enabled    = true
+#    # logging_level        = "ERROR" # requires cloud watch setup
+#    throttling_burst_limit = 10
+#    throttling_rate_limit  = 1
+#  }
 
-  depends_on = [aws_api_gateway_stage.stage]
-}
+#  depends_on = [aws_api_gateway_stage.stage]
+#}
