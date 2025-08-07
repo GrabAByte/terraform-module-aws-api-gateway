9ba05918aa58ded36929776a4cda548da1f35dca
Tom Richards <twr.devops@gmail.com>
2025-08-07 22:28:32 +0100

    updates

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_api_gateway_resource" "resource" {
 resource "aws_api_gateway_method" "method" {
   for_each      = var.api_routes
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
+  resource_id   = aws_api_gateway_resource.resource["each.key"].id
   http_method   = each.value.http_method
   authorization = each.value.api_authorization_method
   authorizer_id = aws_api_gateway_authorizer.lambda_auth.id
