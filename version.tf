e15cc3607de976e021a8cb6b6b27795994ad1404
Tom Richards <twr.devops@gmail.com>
2025-08-08 22:22:24 +0100

    Add releaserc for semantic release

--- version.tf
+++ version.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.13.1"
+    }
   }
 
   # ubuntu 22.04 used as github runner as it has terraform baked
